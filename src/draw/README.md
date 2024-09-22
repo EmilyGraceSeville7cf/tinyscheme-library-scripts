94851cbf25ce0683d61001589fb9093081a8a96b
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-09-23 09:39:26 +1000

    feat(script): add draw utils

---
+++ src/draw/README.md
@@ -0,0 +1,8 @@
+# 📖 Description
+
+Provide utils for drawing.
+
+## 📷 Screenshots
+
+Screenshots are unavailable for this type of script. Consult it's [source code](./draw.scm)
+for the provided functionality.
---
+++ src/draw/draw.scm
@@ -0,0 +1,57 @@
+(define (--draw-not-in-range? value description from to)
+    (cond
+        ((or (< value from) (> value to))
+            (gimp-message (string-append description " is not in range " (number->string from) ".." (number->string to)))
+            #t)
+        (else #f)
+    )
+)
+
+(define (draw-rectangle
+	image
+	layer
+	from-x from-y
+	to-x to-y
+	fill-color)
+	
+	(cond
+		((--draw-not-in-range? from-x "The initial X coordinate" 0 to-x) #f)
+		((--draw-not-in-range? from-y "The initial Y coordinate" 0 to-y) #f)
+		(else
+			(gimp-context-push)
+			(gimp-image-select-rectangle
+				image
+				CHANNEL-OP-REPLACE
+				from-x from-y
+				(- to-x from-x) (- to-y from-y))
+			(gimp-context-set-background fill-color)
+			(gimp-drawable-edit-fill layer FILL-BACKGROUND)
+			(gimp-context-pop)
+		)
+	)
+)
+
+(define (draw-ellipse
+	image
+	layer
+	from-x from-y
+	to-x to-y
+	fill-color)
+	
+	(cond
+		((--draw-not-in-range? from-x "The initial X coordinate" 0 to-x) #f)
+		((--draw-not-in-range? from-y "The initial Y coordinate" 0 to-y) #f)
+		(else
+			(gimp-context-push)
+			(gimp-image-select-ellipse
+				image
+				CHANNEL-OP-REPLACE
+				from-x from-y
+				(- to-x from-x) (- to-y from-y))
+			(gimp-context-set-background fill-color)
+			(gimp-drawable-edit-fill layer FILL-BACKGROUND)
+			(gimp-context-pop)
+		)
+	)
+)
+
